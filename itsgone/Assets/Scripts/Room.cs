c2055ea822333317c31491193febc960d4cee320
levyofun <58773852+levyofun@users.noreply.github.com>
2021-04-02 02:13:40 +0700

    generation stuff

---
+++ itsgone/Assets/Scripts/Room.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public GameObject DoorU;
+    public GameObject DoorR;
+    public GameObject DoorD;
+    public GameObject DoorL;
+    public GameObject DoorF;
+    public GameObject DoorB;
+    public void RotateRandomly()
+    {
+        int count = Random.Range(0, 4);
+
+        for (int i = 0; i < count; i++)
+        {
+            transform.Rotate(0, 90, 0);
+
+            GameObject tmp = DoorL;
+            DoorL = DoorB;
+            DoorB = DoorR;
+            DoorR = DoorF;
+            DoorF = tmp;
+        }
+    }
+}
