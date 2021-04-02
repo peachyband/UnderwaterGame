9ce53712509e53b00d3ac59fb7c264202f5aee2c
CATANYASH <71903281+CATANYASH@users.noreply.github.com>
2021-04-02 18:21:26 +0700

    KLUKALO

--- itsgone/Assets/Scripts/PlayerScript.cs
+++ itsgone/Assets/Scripts/PlayerScript.cs
@@ -5,10 +5,11 @@ using UnityEngine.SceneManagement;
 public class PlayerScript : MonoBehaviour
 {
     private CharacterController charCntrl;
+    private float velocityY = 0f;
 
     [Header("Player characterisic")]
     public float speed = 6.0f;
-    public float jumpSpeed = 8.0f;
+    public float jumpSpeed = 23.0f;
     public float gravity = 20.0f;
     public int lives = 5;
 
@@ -28,8 +29,14 @@ public class PlayerScript : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
         Vector3 dir = transform.right * moveX + transform.forward * moveZ;
+        if (charCntrl.isGrounded && velocityY < 0) velocityY = 0;
+        velocityY += -9.81f * Time.deltaTime;
+
+        if (charCntrl.isGrounded && Input.GetButtonDown("Jump"))
+            velocityY += jumpSpeed * Time.deltaTime * 100;
 
         charCntrl.Move(dir * speed * Time.deltaTime);
+        charCntrl.Move(new Vector3 (0, velocityY, 0) * Time.deltaTime);
     }
 
 }
