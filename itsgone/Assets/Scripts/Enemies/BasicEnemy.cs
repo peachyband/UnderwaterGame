1b492c35805024a54ef9d4e4c6dc710e5b51772c
CATANYASH <71903281+CATANYASH@users.noreply.github.com>
2021-04-04 04:22:29 +0700

    combat

--- itsgone/Assets/Scripts/Enemies/BasicEnemy.cs
+++ itsgone/Assets/Scripts/Enemies/BasicEnemy.cs
@@ -4,25 +4,39 @@ using UnityEngine;
 
 public class BasicEnemy : MonoBehaviour
 {
+    public int health = 100;
+
+    private Rigidbody rig;
+
     public float a = 2f;
     public float b = 3f;
     public float speed = 15f;
     public float t = 0;
     private Vector3 sPos;
+    private Vector3 velocity = Vector3.zero;
 
     private float x = 0;
     private float y = 0;
     private float z = 0;
     public int dir = 1;
+
+    private Transform player;
     // Start is called before the first frame update
     void Start()
     {
         //sPos = transform.position;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rig = this.gameObject.GetComponent<Rigidbody>();
+        rig.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (health <= 0)
+            Destroy(this.gameObject);
+
         //transform.position = sPos + new Vector3(x, y, z);
 
         //t = Mathf.Clamp(t, 0f, 2 * Mathf.PI);
@@ -30,17 +44,19 @@ public class BasicEnemy : MonoBehaviour
         y = a * Mathf.Sin(t);
         z = b * t;*/
 
-        x = -a * Mathf.Sin(t);
+        /*x = -a * Mathf.Sin(t);
         y = a * Mathf.Cos(t);
         z = b;
 
         transform.Translate(x, y, z);
 
-        t += speed * Time.deltaTime * dir;
+        t += speed * Time.deltaTime * dir;*/
+
+        //transform.position = Vector3.SmoothDamp(transform.position, player.position, ref velocity, 0.3f);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        dir = -dir;
+        //dir = -dir;
     }
 }
--- itsgone/Assets/Scripts/weapon.cs
+++ itsgone/Assets/Scripts/weapon.cs
@@ -2,33 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public class weapon : MonoBehaviour
 {
+    public int damage = 20;
     public bool isAttacking = false;
+    public bool isAtt = false;
+    public Transform hand;
+    public Transform end;
+    public float force = 5f;
+    public int dir = -1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //hand = gameObject.GetComponentInParent<Transform>();
+        transform.position = hand.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetButtonDown("Fire1") && !isAtt)
         {
+            isAtt = true;
             isAttacking = true;
+            //transform.Translate(transform.forward * force * dir);
+            transform.position = end.position;
+            StartCoroutine("coolDown");
         }
     }
+
+
 
     private void OnTriggerStay(Collider coll)
     {
         if ((coll.gameObject.tag == "Enemy") && isAttacking)
         {
             //Destroy(coll.gameObject);
-            Debug.Log("ATTACK");
+            //coll.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+            //Debug.Log("ATTACK");
             isAttacking = false;
+            StartCoroutine(glow(coll.gameObject.GetComponent<MeshRenderer>().material));
+            coll.gameObject.GetComponent<Rigidbody>().AddForce(transform.up * force * Time.deltaTime, ForceMode.Impulse);
+            coll.gameObject.GetComponent<BasicEnemy>().health -= damage;
         }
+    }
+
+    IEnumerator coolDown() 
+    {
+        yield return new WaitForSeconds(0.5f);
+        //isAttacking = false;
+
+        transform.position = hand.position;
+        yield return new WaitForSeconds(1.0f);
+        
+        isAtt = false;
+
+    }
+
+    IEnumerator glow(Material mat)
+    {
+        Color orig = mat.GetColor("_Color");
+        //while (true)
+        //{
+            mat.color = Color.red;
+            yield return new WaitForSeconds(0.7f);
+            mat.color = orig;
+        //}
+        
+        //isAttacking = false;
+
+
     }
 
 }
